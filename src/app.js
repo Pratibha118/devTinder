3e93beee3b3b10361c5a73c462dcf05d87a1fadd
Pratibha <Pratibhakaushik001@gmail.com>
2025-11-02 17:47:44 +0530

    Error handling

--- src/app.js
+++ src/app.js
@@ -3,65 +3,32 @@ const { adminAuth } = require('../middlewares/auth');
 
 const app = express();
 
-
-app.use('/admin',adminAuth)
-
-// app.use("/",(req,res,next) =>{
-//     console.log('first')
-//     next();
-// })
-app.get('/multi',(req,res,next) =>{
-    console.log('second')
-    res.send("Hello from multi...2")
-}
-)
-
-app.get('/admin/getData',(req,res,next) =>{
-    console.log('second')
-    res.send("Data send")
-}
-)
-
 //request handler
-app.get("/",(req,res) =>{
-    res.send("Hello from server...")
+
+app.use('/', (err, req, res, next) => {
+    if (err) {
+        res.status(500).send('error occured')
+    }
 })
 
-//get query params while adding params in url
-app.get("/hello",(req,res) =>{
-    console.log(req.query)
-    res.send("Hello...")
+app.use('/getDataUser', (req,res) => {
+    try {
+        throw new Error();
+    } catch (e) {
+        res.status(500).send('something went wrong')
+    }
+
 })
 
-//get dymanic routes
-app.get("/user/:userID/:name",(req,res) =>{
-    console.log(req.params)
-    res.send("parmas...")
+
+//wild card error handler
+app.use('/', (err, req, res, next) => {
+    if (err) {
+        res.status(500).send('error occured')
+    }
 })
 
-//regex
-app.get(/.*fly$/,(req,res) =>{
-    res.send("parmas...")
-})
 
-//regex
-app.get(/a/,(req,res) =>{
-    res.send("simple parmas...")
-})
-
-app.post("/add",async (req,res) =>{
-    console.log('post call ')
-    res.send("Hello...post")
-})
-
-app.delete("/delete",async (req,res) =>{
-    console.log('delete call ')
-    res.send("Hello...delete")
-})
-
-app.get("/test",(req,res) =>{
-    res.send("testing code...")
-})
 
 //Server is listening at port 3000, so that outer world can connect us
 app.listen(3000, () => {
