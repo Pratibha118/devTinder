fe678aa0a2d23d6581ff74c83c76db4f0f030133
Pratibha <Pratibhakaushik001@gmail.com>
2025-11-08 17:42:03 +0530

    created post, get, delete, patch apis and added ,fetched and modified data in db

--- src/app.js
+++ src/app.js
@@ -1,22 +1,81 @@
 const express = require('express');
-const {connectDB} = require('./config/database');
+const { connectDB } = require('./config/database');
 const User = require('./model/user')
 const app = express();
 
+app.use(express.json())
 
-app.post('/signUp', async (req,res)=>{
-    const user = new User({
-        firstName : 'Pratibha',
-        lastName: 'Kaushik',
-        emailId: 'pratibha@gmail.com',
-        password: 'pratibha@123',
-    })
+app.post('/signUp', async (req, res) => {
+
+    const user = new User(req.body)
+
+    try {
+        await user.save();
+        res.send('Saved data succesfully')
+    } catch (err) {
+        res.status(400).send('Error while saving the new user', err.message);
+    }
+})
+
+app.get('/user', async (req, res) => {
+    const userEmail = req.body.emailId;
+    try {
+        const users = await User.find({ emailId: userEmail })
+        if (users.length === 0)
+            res.status(404).send('user not found');
+        else {
+            res.send(users)
+        }
+    } catch (err){
+        res.send('Somethind went wrong');
+    }
+
+})
+
+app.get('/feed', async (req, res) => {
+    try {
+        const users = await User.find({})
+        res.send(users)
+    }
+    catch (err){
+        res.status(400).send('Something went wrong')
+    }
+})
+
+app.delete('/user', async (req,res)=>{
+    const userId = req.body.userId;
 
     try{
-    await user.save();
-    res.send('Saved data succesfully')
-    }catch(err){
-        res.status(400).send('Error while saving the new user', err.message);
+        const user =await User.findByIdAndDelete(userId)
+        res.send('Deleted user successfullys')
+    }catch (err){
+       res.status(400).send('Something went wrong')
+    }
+})
+
+app.patch('/user', async(req,res)=>{
+    const userdID = req.body.userId;
+    const data = req.body;
+
+    try{
+       const user = await User.findByIdAndUpdate('pratibha@gmail.com',data,{returnDocument: 'after'})
+       console.log(user)
+       res.send('User updated successfully')
+    }catch (err){
+       res.status(400).send('Something went wrong')
+    }
+})
+
+app.patch('/user/email', async(req,res)=>{
+    const email = req.body.emailId;
+    const data = req.body;
+
+    try{
+       const user = await User.findOneAndUpdate({emailId: email},data,{returnDocument: 'after'})
+       console.log(user)
+       res.send('User updated by email successfully')
+    }catch (err){
+       res.status(400).send('Something went wrong')
     }
 })
 
